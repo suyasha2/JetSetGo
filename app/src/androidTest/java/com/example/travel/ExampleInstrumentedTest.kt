751716707e0c590350be482d1a1cb5ba4be686c3
suyasha2 <suyashaneupane64@gmail.com>
2026-03-02 16:21:27 +0545

    InstrumentalTesting

--- app/src/androidTest/java/com/example/travel/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/travel/ExampleInstrumentedTest.kt
@@ -1,24 +1,49 @@
 package com.example.travel
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import com.example.travel.view.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class TravelAppNavTest {
+
+    @get:Rule
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.travel", appContext.packageName)
+    fun testLoginToRegister_Navigation() {
+
+        composeRule.waitForIdle()
+        composeRule.onNodeWithText("Register", ignoreCase = true).performClick()
+        composeRule.onNodeWithText("Create Account", ignoreCase = true).assertIsDisplayed()
+    }
+
+    @Test
+    fun testLoginToForgotPassword_Navigation() {
+        composeRule.waitForIdle()
+
+        // "Forgot Password"
+        composeRule.onNodeWithText("Forgot Password", ignoreCase = true).performClick()
+
+        // Verify Forgot Password screen
+        composeRule.onNodeWithText("Reset Password", ignoreCase = true).assertIsDisplayed()
+    }
+
+    @Test
+    fun testLoginInput_And_Click() {
+        composeRule.waitForIdle()
+
+
+        composeRule.onNodeWithText("Email").performTextInput("test@travel.com")
+        composeRule.onNodeWithText("Password").performTextInput("password123")
+
+        // Login button
+        composeRule.onNodeWithText("Login", ignoreCase = true).performClick()
+
+        composeRule.waitForIdle()
     }
 }
